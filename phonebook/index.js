11529f15febba14d5f78ef2ca8e2d68171838713
Malaz Natouf <malaznatouf@gmail.com>
2025-11-29 17:28:56 +0100

    uibuild

--- phonebook/index.js
+++ phonebook/index.js
@@ -9,6 +9,8 @@ morgan.token('type',function (req, res) { return JSON.stringify(req.body) })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
 
+app.use(express.static('dist'))
+
 let persons = [
     { 
       "id": "1",
@@ -95,7 +97,7 @@ app.post('/api/persons',(request, response)=>{
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
